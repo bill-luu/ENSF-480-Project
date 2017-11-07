83635153b22f315ef40a3cc39647f2c2176c6869
Anil_Sood <asood11@outlook.com>
2017-11-07 15:49:58 -0700

    Added empty Database Controller

---
+++ src/business/DatabaseController.java
@@ -0,0 +1,87 @@
+package business;
+
+import java.util.*;
+
+import data.*;
+
+public class DatabaseController 
+{
+	
+	final String PRODUCT_FILE = "";
+	final String ASSIGNMENT_FILE = "";
+	final String DEVELOPER_FILE = "";
+	final String MANAGER_FILE = "";
+	final String LOGIN_FILE = "";
+	final String BUG_FILE = "";
+	
+	public ArrayList<Product>readProductFile()
+	{
+		ArrayList<Product> temp = new ArrayList<Product>();
+		return temp;
+	}
+	
+	public ArrayList<Assignment>readAssignmentFile()
+	{
+		ArrayList<Assignment> temp = new ArrayList<Assignment>();
+		return temp;
+	}
+	
+	public ArrayList<Developer>readDeveloperFile()
+	{
+		ArrayList<Developer> temp = new ArrayList<Developer>();
+		return temp;
+	}
+	
+	public ArrayList<Manager>readManagerFile()
+	{
+		ArrayList<Manager> temp = new ArrayList<Manager>();
+		return temp;
+	}
+	
+	public ArrayList<String>readLoginInfoFile()
+	{
+		ArrayList<String> temp = new ArrayList<String>();
+		return temp;
+	}
+	
+	public ArrayList<Bug>readBugFile()
+	{
+		ArrayList<Bug> temp = new ArrayList<Bug>();
+		return temp;
+	}
+	
+	public void writeReport()
+	{
+		
+	}
+	
+	public void writeProductFile(ArrayList<Product> products)
+	{
+		
+	}
+	
+	public void writeAssignmentFile(ArrayList<Assignment> assignments)
+	{
+		
+	}
+	
+	public void writeDeveloperFile(ArrayList<Developer> developers)
+	{
+		
+	}
+	
+	public void writeManagerFile(ArrayList<Manager> managers)
+	{
+		
+	}
+	
+	public void writeLoginInfoFile(ArrayList<String> loginInfo)
+	{
+		
+	}
+	
+	public void writeBugFile(ArrayList<Bug> bugs)
+	{
+		
+	}
+}
