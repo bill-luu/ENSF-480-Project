f16c48ac52dbd8ce1e8ba7926dfbf78a5b2f8168
BKDuncan <bailey.duncan@ucalgary.ca>
2017-11-06 21:01:32 -0700

    Commiting just the java files and packages without any eclipse project

---
+++ data/DatabaseController.java
@@ -0,0 +1,157 @@
+package data;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ */
+public class DatabaseController {
+
+    /**
+     * Default constructor
+     */
+    public DatabaseController() {
+    }
+
+    /**
+     * 
+     */
+    private final String PRODUCT_FILE = "";
+
+    /**
+     * 
+     */
+    private final String ASSIGNMENT_FILE = "";
+
+    /**
+     * 
+     */
+    private final String DEVELOPER_FILE = "";
+
+    /**
+     * 
+     */
+    private final String MANAGER_FILE = "";
+
+    /**
+     * 
+     */
+    private final String LOGIN_FILE = "";
+
+    /**
+     * 
+     */
+    private final String BUG_FILE = "";
+
+
+    /**
+     * @param void 
+     * @return
+     */
+    public ArrayList<Product> readProductFile() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param void 
+     * @return
+     */
+    public ArrayList<Assignment> readAssignmentFile() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param void 
+     * @return
+     */
+    public ArrayList<Developer> readDeveloperFile() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param void 
+     * @return
+     */
+    public ArrayList<Manager> readManagerFile() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param void 
+     * @return
+     */
+    public ArrayList<String> readLoginInfoFile() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param void 
+     * @return
+     */
+    public ArrayList<Bug> readBugFile() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param reportToWrite 
+     * @return
+     */
+//    public void writeReport(Report reportToWrite) {
+//        // TODO implement here
+//    }
+
+    /**
+     * @param ArrayList product 
+     * @return
+     */
+    public void writeProductFile(ArrayList product) {
+        // TODO implement here
+    }
+
+    /**
+     * @param ArrayList Assignment 
+     * @return
+     */
+    public void writeAssignmentFile(ArrayList Assignment) {
+        // TODO implement here
+    }
+
+    /**
+     * @param ArrayList developer 
+     * @return
+     */
+    public void writeDeveloperFile(ArrayList developer) {
+        // TODO implement here
+    }
+
+    /**
+     * @param ArrayList manager 
+     * @return
+     */
+    public void writeManagerFile(ArrayList manager) {
+        // TODO implement here
+    }
+
+    /**
+     * @param ArrayList login 
+     * @return
+     */
+    public void writeLoginFile(ArrayList login) {
+        // TODO implement here
+    }
+
+    /**
+     * @param ArrayList bug 
+     * @return
+     */
+    public void writeBugFile(ArrayList bug) {
+        // TODO implement here
+    }
+
+}
