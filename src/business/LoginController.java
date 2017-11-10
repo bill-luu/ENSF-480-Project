9466fe9b67a3cbbb618711927f585ae178933023
BKDuncan <bailey.duncan@ucalgary.ca>
2017-11-09 18:26:44 -0700

    Added constructor and comments. The class is still incomplete.

--- src/business/LoginController.java
+++ src/business/LoginController.java
@@ -3,32 +3,45 @@ import java.util.*;
 import data.Employee;
 
 /**
- * 
+ * LoginController is a class that authenicates user logins
  */
 public class LoginController {
+
+	/**
+     * An arraylist of all logins and passwords
+     */
+    private ArrayList<String> loginInfo_;
 
     /**
      * Default constructor
      */
     public LoginController() {
+    	loginInfo_ = new ArrayList<String>();
+    }
+    
+    /**
+     *  Constructor that sets the login list
+     */
+    public LoginController(ArrayList<String> logins) {
+    	loginInfo_ = logins;
     }
 
     /**
-     * 
-     */
-    private ArrayList<String> loginInfo_;
-
-
-    /**
-     * @param login 
-     * @return
+     * @param login The login information being authenticated
+     * @return An employee object if the login is valid (Developer or Manager). Null if not valid.
      */
     public Employee validateLogin(String login) {
-        // TODO implement here
+    	// For-each login in the list
+        for(String validLogin : loginInfo_){
+        	if(validLogin.matches(login))
+			{
+        		// TODO: Get Employee Object and Return it. How does LoginController access the manager/developer list?
+			}
+        }
         return null;
     }
 
-
+    
 	public ArrayList<String> getLoginInfo_() {
 		return loginInfo_;
 	}
